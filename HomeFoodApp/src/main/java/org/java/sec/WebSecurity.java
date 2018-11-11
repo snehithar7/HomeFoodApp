2769909dfdb803cd1290aec4618a5fd0bf84d5eb
snehitha <snehithar7@gmail.com>
2018-11-11 17:59:30 -0500

    Web security page and html page

--- HomeFoodApp/src/main/java/org/java/sec/WebSecurity.java
+++ HomeFoodApp/src/main/java/org/java/sec/WebSecurity.java
@@ -1,15 +1,40 @@
 package org.java.sec;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class WebSecurity extends  WebSecurityConfigurerAdapter {
+	//Bcrypt will generate automatic salt number for encoding of password
+	@Bean
+	public PasswordEncoder  passwordEncoder() {
+		return new  BCryptPasswordEncoder();	
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws  Exception{
+		http.csrf().disable();
 		http.authorizeRequests()
 		    .antMatchers("/","/home")
 		    .permitAll();
 		
+    http.authorizeRequests().antMatchers("/admin/orderList", "/admin/order", "/admin/accountInfo")//
+         .access("hasAnyRole('ROLE_USER', 'ROLE_CHEF')");
+
+   // Pages only for CHEF
+   http.authorizeRequests().antMatchers("/admin/product").access("hasRole('ROLE_CHEF')");
+   http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+   http.authorizeRequests().and().formLogin()
+   .loginProcessingUrl("/check")
+   .loginPage("admin/login")
+   .defaultSuccessUrl("/admin/accountInfo")
+   .failureUrl("/admin/login")
+   .usernameParameter("userName")
+   .passwordParameter("password")
+   .and().logout().logoutUrl("/admin/logout").logoutSuccessUrl("/");
 	}
 
-}
+	}
+
+
