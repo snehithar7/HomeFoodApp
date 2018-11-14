11acb4b2bf025c9592e7669b32a150135ab7e398
snehitha <snehithar7@gmail.com>
2018-11-13 23:41:40 -0500

    Added Html pages and java classes of customer and Item

---
+++ HomeFoodApp/src/main/java/org/java/model/CustomerInfo.java
@@ -0,0 +1,60 @@
+package org.java.model;
+
+import org.java.form.CustomerForm;
+
+public class CustomerInfo {
+	
+private String name;
+private String email;
+private String address;
+private String phone;
+private Boolean valid;
+
+public CustomerInfo() {
+
+}
+public CustomerInfo(CustomerForm customerForm) {
+
+	
+	this.name = customerForm.getName();
+	    this.address = customerForm.getAddress();
+	    this.email = customerForm.getEmail();
+	    this.phone = customerForm.getPhone();
+	    this.valid = customerForm.isValid();
+	    }
+
+
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getPhone() {
+	return phone;
+}
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+public Boolean getValid() {
+	return valid;
+}
+public void setValid(Boolean valid) {
+	this.valid = valid;
+}
+	 
+}
+
+
